a88ed3aebc5043717d5e1c05d4edcc8013e67eae
kiramoake140 <kiramoake140@gmail.com>
2025-03-07 04:16:25 +0000

    Add 2.js

---
+++ src/2.js
@@ -0,0 +1,7 @@
+// Get a random number between 1 and 10
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+// Print the result to the console
+console.log(getRandomNumber());
