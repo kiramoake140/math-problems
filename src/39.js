ec665f57683d429ac4454c1b7322671338db2563
kiramoake140 <kiramoake140@gmail.com>
2025-05-23 05:35:44 +0000

    Add 39.js

---
+++ src/39.js
@@ -0,0 +1,10 @@
+function fibonacci(n) {
+  if (n <= 1) return n;
+  let prev = 0, curr = 1;
+  for (let i = 2; i <= n; i++) {
+    [prev, curr] = [curr, prev + curr];
+  }
+  return curr;
+}
+
+console.log(fibonacci(10));
